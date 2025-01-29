91dfb0193130ab2a2b7bc08e259beb3c2f067967
frrst-ian <frrstxd@proton.me>
2025-01-29 11:35:19 +0800

    feat: add load and error message in fetching api
    implement loading and error message functionality to provide a more clear info

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import "./Shop.css";
 export const Shop = () => {
   const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetchCards();
@@ -13,14 +15,21 @@ export const Shop = () => {
       const response = await fetch(
         "https://fakestoreapi.com/products/category/electronics?limit=10"
       );
+      if (!response.ok) {
+        throw new Error("Failed to fetch products");
+      }
       const data = await response.json();
-
+      setLoading(false);
       setCards(data);
       console.log(data);
     } catch (error) {
       console.error("Error fetch", error);
+      setError(error.message);
+      setLoading(false);
     }
   };
+  if (loading) return <div className="load-txt">Loading products...</div>;
+  if (error) return <div>Error: {error}</div>;
   return (
     <div className="card-list">
       <h1>Electriko Shop</h1>
